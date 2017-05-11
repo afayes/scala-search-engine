007c890b394b78f48464bc683c88f4e65048a7cf
abulfayes <afayes@hotmail.co.uk>
2017-05-11 20:15:09 +0100

    search code - WIP

--- hlv-crawler/src/main/scala/com/ixxus/temp.scala
+++ hlv-crawler/src/main/scala/com/ixxus/temp.scala
@@ -6,11 +6,15 @@ package com.ixxus
 // todo delete
 object temp extends App {
 
-  val htmlPattern = ("<html.*>.*").r
-  //val htmlPattern = "<html.*>.*".r
-  //val string = "a c c<html>\n<html lang=\"en-GB\" id=\"responsive-news\">aaaaa    </html>"
-  //val string = "<!DOCTYPE html>\n<!--[if lte IE 9]>\n  <html lang=\"en-GB\" class=\"no-js no-flexbox no-flexboxlegacy\">\n<![endif]-->\n<!--[if gt IE 9]><!-->\n  <html lang=\"en-GB\" class=\"no-js\">\n<!--<![endif]-->\n<head> </html>"
-  val string = "<!DOCTYPE html>\n<!--[if lte IE 9]>\n  <html lang=\"en-GB\" class=\"no-js no-flexbox no-flexboxlegacy\">\n<![endif]-->\n<!--[if gt IE 9]><!-->\n  <html lang=\"en-GB\" class=\"no-js\">\n<!--<![endif]-->\n<head> </html>"
-  println(htmlPattern.findFirstIn(string).get)
+//  val htmlPattern = ("<html.*>.*").r
+//  //val htmlPattern = "<html.*>.*".r
+//  //val string = "a c c<html>\n<html lang=\"en-GB\" id=\"responsive-news\">aaaaa    </html>"
+//  //val string = "<!DOCTYPE html>\n<!--[if lte IE 9]>\n  <html lang=\"en-GB\" class=\"no-js no-flexbox no-flexboxlegacy\">\n<![endif]-->\n<!--[if gt IE 9]><!-->\n  <html lang=\"en-GB\" class=\"no-js\">\n<!--<![endif]-->\n<head> </html>"
+//  val string = "<!DOCTYPE html>\n<!--[if lte IE 9]>\n  <html lang=\"en-GB\" class=\"no-js no-flexbox no-flexboxlegacy\">\n<![endif]-->\n<!--[if gt IE 9]><!-->\n  <html lang=\"en-GB\" class=\"no-js\">\n<!--<![endif]-->\n<head> </html>"
+//  println(htmlPattern.findFirstIn(string).get)
+
+  val pattern = "word1.* word2.".r
+  val text = "hello word1 word2 hello2 hellow word1 word1 word2"
+  println(pattern.findAllMatchIn(text).size)
 
 }
