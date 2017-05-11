697f1ab48be22fde26a5a8fcf6d052c1c4820a86
abulfayes <afayes@hotmail.co.uk>
2017-05-11 20:20:13 +0100

    commit files - not sure why they were not committed!!!

---
+++ hlv-search/src/test/scala/com/ixxus/hastalavista/search/MyFirstTest.scala
@@ -0,0 +1,12 @@
+package com.ixxus.hastalavista.crawler
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by mirko on 08/05/2017.
+  */
+class MyFirstTest extends FlatSpec with Matchers{
+    "my unit test" should "work properly" in {
+        true should be(true)
+    }
+}
