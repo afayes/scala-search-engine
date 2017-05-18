12065355a4f491062241b088c50ee3c9b3679cc8
abulfayes <afayes@hotmail.co.uk>
2017-05-18 14:27:01 +0100

    refactor cake pattern impl to make it possible to access inner trait

--- hlv-search/src/main/scala/com/ixxus/hastalavista_refactor/analytic/AnalyticsServiceComponent.scala
+++ hlv-search/src/main/scala/com/ixxus/hastalavista_refactor/analytic/AnalyticsServiceComponent.scala
@@ -2,58 +2,52 @@ package com.ixxus.hastalavista_refactor.analytic
 
 import java.util.Date
 
-import com.ixxus.hastalavista.search.model.AnalyticQueryItem
-
 /**
   * todo add comments.
   */
 trait AnalyticsServiceComponent {
 
     val analyticsService: AnalyticsService
+}
 
-    trait AnalyticsService {
-        def updateLastRetrievalDate(url: String, date: Date)
+trait AnalyticsService {
+    def updateLastRetrievalDate(url: String, date: Date)
 
-        def updateClickCount(url: String)
+    def updateClickCount(url: String)
 
-        def apply(url: String): Analytics
+    def apply(url: String): Analytics
 
-        def getAll(): Seq[Analytics]
-    }
+    def getAll(): Seq[Analytics]
+}
 
-    class AnalyticServiceUsingHashMap extends AnalyticsService {
+class AnalyticServiceUsingHashMap extends AnalyticsService {
 
-        override def updateLastRetrievalDate(url: String, date: Date): Unit = {
-            val existing = AnalyticsData.data.get(url)
-            existing match {
-                case Some(existingItem) =>
-                    val existingRetrievalDate = existingItem.lastRetrievalDate
-                    if (existingRetrievalDate == null) update(Analytics(url, date, existingItem.clickCount))
-                    else if (date.compareTo(existingRetrievalDate) > 0) update(Analytics(url, date, existingItem.clickCount))
+    override def updateLastRetrievalDate(url: String, date: Date): Unit = {
+        val existing = AnalyticServiceUsingHashMap.data.get(url)
+        existing match {
+            case Some(existingItem) =>
+                val existingRetrievalDate = existingItem.lastRetrievalDate
+                if (existingRetrievalDate == null) update(Analytics(url, date, existingItem.clickCount))
+                else if (date.compareTo(existingRetrievalDate) > 0) update(Analytics(url, date, existingItem.clickCount))
 
-                case None => update(Analytics(url, date, 0))
-            }
+            case None => update(Analytics(url, date, 0))
         }
-
-        private def update(analytics: Analytics): Unit = AnalyticsData.data = AnalyticsData.data + (analytics.url -> analytics)
-
-
-        override def updateClickCount(url: String): Unit = ???
-
-        override def apply(url: String): Analytics = ???
-
-        override def getAll(): Seq[Analytics] = AnalyticsData.data.values.toSeq
-
     }
 
-    object AnalyticServiceUsingHashMap {
-        def apply():AnalyticServiceUsingHashMap = new AnalyticServiceUsingHashMap
-    }
+    private def update(analytics: Analytics): Unit = AnalyticServiceUsingHashMap.data = AnalyticServiceUsingHashMap.data + (analytics.url -> analytics)
+
+
+    override def updateClickCount(url: String): Unit = ???
+
+    override def apply(url: String): Analytics = ???
+
+    override def getAll(): Seq[Analytics] = AnalyticServiceUsingHashMap.data.values.toSeq
 
 }
 
-object AnalyticsData {
-    var data = Map[String, Analytics]()
+object AnalyticServiceUsingHashMap {
+    private var data = Map[String, Analytics]()
+    def apply():AnalyticServiceUsingHashMap = new AnalyticServiceUsingHashMap
 }
 
 
--- hlv-search/src/main/scala/com/ixxus/hastalavista_refactor/search/PageIndexComponent.scala
+++ hlv-search/src/main/scala/com/ixxus/hastalavista_refactor/search/PageIndexComponent.scala
@@ -6,38 +6,34 @@ import org.slf4j.LoggerFactory
   * todo add comments.
   */
 trait PageIndexComponent {
-
-  trait PageIndex {
-    def update(url:String, page:Page)
-    def apply(url:String):Option[Page]
-    def getAll():Seq[Page]
-  }
-
   val pageIndex:PageIndex
-
-  class PageIndexUsingHashMap extends PageIndex {
-
-    private val logger = LoggerFactory.getLogger(classOf[PageIndexUsingHashMap])
-
-    override def update(url: String, page: Page): Unit = {
-      PageIndexData.data = PageIndexData.data + (url -> page)
-      logger.debug(s"Added ${page.url} to index")
-    }
-
-    override def apply(url: String): Option[Page] = {
-      PageIndexData.data.get(url)
-    }
-
-    override def getAll(): Seq[Page] = PageIndexData.data.values.toSeq
-  }
-
-  object PageIndexUsingHashMap {
-    def apply():PageIndexUsingHashMap = new PageIndexUsingHashMap
-  }
 }
 
-object PageIndexData {
+trait PageIndex {
+  def update(url:String, page:Page)
+  def apply(url:String):Option[Page]
+  def getAll():Seq[Page]
+}
+
+class PageIndexUsingHashMap extends PageIndex {
+
+  private val logger = LoggerFactory.getLogger(classOf[PageIndexUsingHashMap])
+
+  override def update(url: String, page: Page): Unit = {
+    PageIndexUsingHashMap.data = PageIndexUsingHashMap.data + (url -> page)
+    logger.debug(s"Added ${page.url} to index")
+  }
+
+  override def apply(url: String): Option[Page] = {
+    PageIndexUsingHashMap.data.get(url)
+  }
+
+  override def getAll(): Seq[Page] = PageIndexUsingHashMap.data.values.toSeq
+}
+
+object PageIndexUsingHashMap {
   var data = Map[String, Page]()
+  def apply():PageIndexUsingHashMap = new PageIndexUsingHashMap
 }
 
 
