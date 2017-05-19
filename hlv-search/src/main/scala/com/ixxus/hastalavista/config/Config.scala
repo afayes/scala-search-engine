92d4f7379359ceca6a5f3c8fb17e51ba8fff90b3
abulfayes <afayes@hotmail.co.uk>
2017-05-19 18:29:23 +0100

    clean up page index component

--- hlv-search/src/main/scala/com/ixxus/hastalavista/config/Config.scala
+++ hlv-search/src/main/scala/com/ixxus/hastalavista/config/Config.scala
@@ -1,6 +1,6 @@
 package com.ixxus.hastalavista.config
 
-import java.util.concurrent.{LinkedBlockingQueue}
+import java.util.concurrent.LinkedBlockingQueue
 
 import com.ixxus.hastalavista.analytics.AnalyticsServiceComponent
 import com.ixxus.hastalavista.search._
@@ -12,7 +12,7 @@ object Config {
 
     val components = new SearchPagesByRelevanceComponent with PageIndexComponent with RetrievePagesByRetrievalDateComponent with AnalyticsServiceComponent {
         override val searchPagesByRelevance = SearchPagesByRelevanceUsingRegEx()
-        override val pageIndex:PageIndex = PageIndexUsingHashMap()
+        override val pageIndex:PageIndex = PageIndexUsingHashMap(Map())
         override val analyticsService:AnalyticsService = AnalyticServiceUsingHashMap(Map(), new LinkedBlockingQueue[String])
         override val retrievePagesByRetrievalDate: RetrievePagesByRetrievalDate = RetrievePagesByRetrievalDateImpl()
     }
