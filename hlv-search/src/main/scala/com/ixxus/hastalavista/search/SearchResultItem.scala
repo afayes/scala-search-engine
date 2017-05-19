e5fa96058615c52ce3db9afe3fc924a8c8c9dcfe
abulfayes <afayes@hotmail.co.uk>
2017-05-19 18:46:47 +0100

    clean up/refactor search result item

--- hlv-search/src/main/scala/com/ixxus/hastalavista/search/SearchResultItem.scala
+++ hlv-search/src/main/scala/com/ixxus/hastalavista/search/SearchResultItem.scala
@@ -7,13 +7,8 @@ import scala.beans.BeanProperty
 /**
   * todo add comments.
   */
-class SearchResultItem(@BeanProperty var url:String, @BeanProperty var matchCount:Int, @BeanProperty var matchDistance:Int, @BeanProperty var lastRetrievalDate:Date, @BeanProperty var creationDate:Date) {
-
-  override def toString = s"SearchResultItem($url, $matchCount, $matchDistance, $lastRetrievalDate, $creationDate)"
-}
-
-object SearchResultItem {
-  def apply(url:String, matchCount:Int, mathDistance:Int, lastRetrievalDate:Date, creationDate:Date):SearchResultItem = {
-    new SearchResultItem(url, matchCount, mathDistance, lastRetrievalDate, creationDate)
-  }
-}
+case class SearchResultItem(@BeanProperty var url:String,
+                            @BeanProperty var matchCount:Int,
+                            @BeanProperty var matchDistance:Int,
+                            @BeanProperty var lastRetrievalDate:Date,
+                            @BeanProperty var creationDate:Date)
